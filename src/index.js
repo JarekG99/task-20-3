fd6f0cafe2827d6c1e33f2f06b40b037af60b923
Jarek Gajdulewicz <j.gajdulewicz@icloud.com>
2018-04-19 12:40:00 +0200

    First commit

--- src/index.js
+++ src/index.js
@@ -2,23 +2,29 @@ import React from 'react';
 import { render }  from 'react-dom';
 import { Provider } from 'react-redux';
 import store from './store';
+import { Router, hashHistory } from 'react-router';
+import routes from './routes';
 
-// import './index.css';
+import './index.css';
+import 'bootstrap/dist/css/bootstrap.css';
+// import 'bootstrap/dist/css/bootstrap-theme.css';
+
 // import App from './App';
 import registerServiceWorker from './registerServiceWorker';
 import DevTools from './DevTools';
-import  {getCountries} from './actions/actions-countries';
 
 render(
   <Provider store={store}>
-    <div className='App'>
-      <h1>List of countries: </h1>
-
-      <DevTools />
-    </div>
+    <Router history={hashHistory} routes={routes} />
   </Provider>,
+    // <div className='App'>
+    //   <h1>List of countries: </h1>
+    //
+    //   <DevTools />
+    // </div>
+
    document.getElementById('root')
  );
-store.dispatch(getCountries());
+// store.dispatch(getCountries());
 
 registerServiceWorker();
---
+++ src/presentational/home.component.js
@@ -0,0 +1,7 @@
+import React from 'react';
+
+const Home = () => (
+    <h1>Welcome to app.: countries.js</h1>
+);
+
+export default Home;
